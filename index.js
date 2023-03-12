3f9848e86f9084e5ed2334758cd6b4b82f437cc5
Gentle Coder <user@MACs-MacBook-Pro.local>
2023-03-12 22:47:27 +0100

    pushed all files

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+const details = {
+    acetec: {
+        name: 'Acetec Concepts Ltd',
+        id: 281,
+        siteName: ['Awka - Bamboo Garden', 'Awka - Impala Lounge & Bar', 'Enugu - Villa Toscana'],
+        siteId: [6734, 7198, 6410],
+        businessDev: 'Roland',
+        state: ['Anambra', 'Anambra', 'Enugu'],
+        city: ['Awka', 'Awka', 'Enugu'],
+        locationType: ['Bar/Club', 'Bar/Club', 'Hotel'],
+        companyShare: 50,
+        model: ['HotFun', 'Energy+', 'classic+']
+    },
+    adenuga: {
+        name: 'Adenuga',
+        id: 185,
+        siteName: ['Benin City - De Casino Royal', 'Benin City - Exquisite Home', 'Benin City - Global Embassy', 'Lagos - Jabita Hotel, Ikeja', 'Lagos - Olowu Str 38', 'Lagos - One Love Hotel', 'Lagos - Pekas 1 Hotel 4 Opebi Rd', 'Pekas 2'],
+        siteId: [6274, 6205, 5726, 5484, 5476, 6699, 7317, 6020],
+        businessDev: ['Abraham', 'John Benin'],
+        state: ['Edo', 'Edo', 'Edo', 'Lagos', 'Lagos', 'Lagos', 'Lagos', 'Lagos'],
+        city: ['Benin', 'Benin', 'Benin', 'Ikeja', 'Ikeja', 'Oshodi', 'Ikeja', 'Ikeja'],
+        locationType: ['Hotel', 'Hotel', 'Bar/Club', 'Gaming Centre', 'Gaming Centre', 'Hotel', 'Bar/Club', 'Bar/Club'],
+        companyShare: 50,
+        model: ['HotFun', 'Energy+', 'classic+', 'Turbo+']
+    },
+}
